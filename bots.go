9f7cc99cd0ef4f446885c8d3e986b8564b93de35
Matt Miller <matt@flashnotes.com>
2013-09-12 19:54:45 -0400

    remove parrot2 and think about refacotoring this

--- bots.go
+++ bots.go
@@ -6,16 +6,10 @@ import "log"
 
 import "github.com/mgmtech/gobots/registry"
 
-// Import the bots
-import parrot "github.com/mgmtech/gobots/parrot2"
-//import burt "github.com/mgmtech/gobots/burt"
-//import webvu "github.com/mgmtech/gobots/webvu"
 
 
 var Registry = registry.BotRegistry{
     "parrot": parrot.Registry,
-//    RegEntry(burt.Registry),
-//    RegEntry(webvu.Registry)
 }
 
 func Start() {
